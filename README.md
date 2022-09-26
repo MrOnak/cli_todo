cc1967a6890631bbae01616c456b3f36ecd6ed01
Dominique Stender <oldaric.gw2@gmail.com>
2022-09-26 08:42:57 +0200

    initial public version of the todo app

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# cli_todo
-todo list manager for zsh
+# zsh commandline todo manager
+
+This is a todo list manager for zsh. It was born out of curiosity and
+wanting to learn coreutils.
+
+## usage
+
+* `todo` lists currently open and completed tasks
+* `todo add some task` adds 'some task' to the list of open tasks
+* `todo done N` marks the Nth task as completed
+* `todo clean` purges completed tasks off the lists
+
+## requirements
+
+Currently this requires zsh due to how I handle colors in the script.
+The zsh color syntax was chosen for convenience, you can replace it with
+color escape codes and get rid of the color autoloading.
+
+It also requires a nerdfont for checkmark glyphs. You can change the 
+glyphs back to ASCII by replacing `GLYPH_TODO` and `GLYPH_DONE`. 
+I recommend 'o' for an open todo and 'x' for a completed one.
+
+## setup 
+
+Both open and completed todos are stored in text files. I do recommend
+creating a directory like `~/.todos` manually and changing the `FILE_DIR`
+variable to an absolute path. Files will be automatically created if they
+don't exist.
+
+You might also want to symlink the `todo` script to `~/.local/bin` or 
+another directory in your `PATH` for ease of access.
+
---
+++ todo.sh
@@ -0,0 +1,44 @@
+#!/bin/zsh
+autoload colors
+colors
+
+FILE_DIR=~/.todos
+FILE_TODO=$FILE_DIR"/todos.txt"
+FILE_DONE=$FILE_DIR"/done.txt"
+if [[ ! -d $FILE_DIR ]]; then echo "directory '$FILE_DIR' not found"; exit 1; fi
+touch $FILE_TODO $FILE_DONE
+GLYPH_TODO="   "
+GLYPH_DONE="       "
+TODOS=`comm $FILE_TODO $FILE_DONE | cut --fields 1 | grep -v -E "(^#|^\s*$)"`
+DONES=`comm $FILE_TODO $FILE_DONE | cut --fields 3 -s | grep -v -E "(^#|^\s*$)"`
+
+if [[ $# -ne 0 ]]; then
+  COMMAND=$1
+  shift
+  case $COMMAND in
+    add|a)
+      echo $* >> $FILE_TODO;
+      cat $FILE_TODO | grep -v -E "(^#|^\s*$)" | sort -u -o $FILE_TODO - ;;
+    done|d)
+      echo $TODOS | awk 'NR=='$1' {print;exit}' >> $FILE_DONE; 
+      cat $FILE_DONE | grep -v -E "(^#|^\s*$)" | sort -u -o $FILE_DONE - ;;
+    clean|c)
+      echo $TODOS > $FILE_TODO
+      echo "# this file contains completed todos" > $FILE_DONE ;;
+    *)
+      echo "Usage: $0 (add|done|clean|)"
+      echo "  $0                  prints current open and completed todos"
+      echo "  $0 add some task    adds 'some task' as open todo"
+      echo "  $0 done N           marks the Nth todo as completed"
+      echo "  $0 clean            clears completed todos off the list" ;;
+  esac
+else
+  echo $fg_bold[default]"  my todos: "$reset_color
+  if [[ -n $TODOS ]]; then 
+    echo $TODOS | nl -s ") " -w2 | sed s/^/$fg[red]$GLYPH_TODO$reset_color/
+  fi
+  if [[ -n $DONES ]]; then
+    echo $DONES | sed s/^/$fg[green]$GLYPH_DONE$reset_color/
+  fi
+fi
+
